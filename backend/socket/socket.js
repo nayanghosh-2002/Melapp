ef3a8db47940fea0d10d55d063864d091b4ca6e5
Nayan Ghosh <138111956+nayanghosh-2002@users.noreply.github.com>
2025-07-20 00:50:21 +0530

    commit

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -8,8 +8,9 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors:{
-        origin:process.env.URL,
-        methods:['GET','POST']
+        origin:`https://melapp-pied.vercel.app`,
+        methods:['GET','POST'],
+        credentials: true,
     }
 })
 
