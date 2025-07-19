a43375dcf57ce22cdf4f50ba765b11f412b844ba
Nayan Ghosh <138111956+nayanghosh-2002@users.noreply.github.com>
2025-07-20 03:07:04 +0530

    commit

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -20,7 +20,7 @@ import storage from 'redux-persist/lib/storage'
 const persistConfig = {
     key: 'root',
     version: 1,
-    blacklist: ['socketio'],
+    storage,
 }
 
 const rootReducer = combineReducers({
